c14fd610c46a8989c689973261c783c846adae54
Christian Schulze <christian.schulze@gmail.com>
2015-10-15 20:56:55 +1100

    def method_a(params = params) # params always nil

--- app/helpers/admin/resources/table_helper.rb
+++ app/helpers/admin/resources/table_helper.rb
@@ -11,7 +11,7 @@ module Admin::Resources::TableHelper
     render "helpers/admin/resources/table", locals
   end
 
-  def table_header(model, fields, params = params)
+  def table_header(model, fields, params = self.params)
     fields.map do |key, value|
 
       key = key.gsub(".", " ") if key.to_s.match(/\./)
--- app/helpers/admin/resources_helper.rb
+++ app/helpers/admin/resources_helper.rb
@@ -1,6 +1,6 @@
 module Admin::ResourcesHelper
 
-  def search(resource = @resource, params = params)
+  def search(resource = @resource, params = self.params)
     if (typus_search = resource.typus_defaults_for(:search)) && typus_search.any?
 
       hidden_filters = params.dup
